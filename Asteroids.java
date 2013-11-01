41abfb59e1e8dd41f28017c6e0a0f233797b5ece
Brad <bradwarrum@gmail.com>
2013-11-01 19:57:40 -0400

    Create Asteroids.java

---
+++ Asteroids.java
@@ -0,0 +1,9 @@
+// Main entry point for the asteroids game. Invokes run call
+// to the static game object.
+public class Asteroids {
+	public static void main(String[] args) {
+		Game.run();
+	}
+	
+
+}
